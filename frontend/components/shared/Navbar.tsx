f53b4f49f359187f2f60031e23871ec8e5ded6f8
Moknine Elhadj <mokninemoknine999@gmail.com>
2024-04-27 22:29:41 +0100

    create Navbar

--- frontend/components/shared/Navbar.tsx
+++ frontend/components/shared/Navbar.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+"use client";
+import Link from "next/link";
+
+import React, { useState } from "react";
+
+import { Menu } from "lucide-react";
+import { MobileNavigation } from "./MobileNavigation";
+import Navigation from "./Navigation";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [open, setOpen] = useState(false);
+
+  return (
+    <header>
+      <div className="max-w-6xl mx-auto p-8">
+        <div className="flex justify-between items-center">
+          <div>
+            <Link href={"/"}>
+              <h2 className="font-bold text-2xl text-primary">Fcources</h2>
+            </Link>
+          </div>
+          <div className="hidden md:flex">
+            <Navigation />
+          </div>
+          <Menu
+            className="md:hidden cursor-pointer"
+            onClick={() => setOpen((prev) => !prev)}
+          />
+          <MobileNavigation open={open} setOpen={setOpen} />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
---
+++ frontend/components/shared/Navigation.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { Button } from "../ui/button";
+const links = [
+  { label: "Home", href: "/" },
+  { label: "About Us", href: "/aboutUs" },
+  { label: "Courses", href: "/courses" },
+];
+const Navigation = () => {
+  const pathname = usePathname();
+  return (
+    <nav className="flex flex-col md:flex-row items-center gap-8">
+      {links.map((link) => (
+        <Link
+          href={link.href}
+          key={link.label}
+          className={`${
+            pathname === link.href && "text-primary font-semibold"
+          }  `}
+        >
+          {link.label}
+        </Link>
+      ))}
+      <Button className="px-8">Contact Us</Button>
+    </nav>
+  );
+};
+
+export default Navigation;
